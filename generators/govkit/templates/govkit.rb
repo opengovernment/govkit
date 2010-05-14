b1ba8fda94e98b0d9b9000c03f8dc3e5243b384e
Srinivas Aki <srinivas.aki@gmail.com>
2010-05-14 15:47:34 -0700

    Deleting old directories Added votesmart api key to generator

--- generators/govkit/templates/govkit.rb
+++ generators/govkit/templates/govkit.rb
@@ -1,9 +1,11 @@
 if defined? GovKit
-
   GovKit.configure do |config|
     # Get an API key for Sunlight's Fifty States project here:
     # http://services.sunlightlabs.com/accounts/register/
     config.fiftystates_apikey = 'YOUR_FIFTYSTATES_API_KEY'
-  end
 
+    ##API key for votesmart
+    #http://votesmart.org/services_api.php
+    config.votesmart_apikey = 'YOUR_VOTESMART_API_KEY'
+  end
 end
