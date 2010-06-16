d122237c732b00a806d03178b2e236ce61667436
Carl Tashian <carl@ppolitics.org>
2010-06-16 16:05:16 -0700

    Raise exceptions correctly on bad API key and bad request

--- lib/gov_kit/follow_the_money.rb
+++ lib/gov_kit/follow_the_money.rb
@@ -2,6 +2,25 @@ module GovKit
   class FollowTheMoneyResource < Resource
     default_params :key => GovKit::configuration.ftm_apikey
     base_uri GovKit::configuration.ftm_base_url
+
+    def self.get_xml(path, options)
+      response = get(path, options)
+      doc = Hpricot::XML(response)
+
+      e = doc.search("//error")
+
+      # API Key invalid
+      if e.size > 0
+        raise case e.first.attributes['code']
+        when "100":
+          GovKit::NotAuthorizedError
+        when "300":
+          GovKit::InvalidRequestError
+        end, e.first.attributes['text']
+      end
+
+      doc
+    end
   end
 
   module FollowTheMoney
@@ -9,12 +28,10 @@ module GovKit
       def self.list
         next_page, result, page_num = "yes", [], 0
 
-        until next_page == "no"
-          puts "Getting batch number #{page_num}"
+        until next_page != "yes"
+          # puts "Getting batch number #{page_num}"
 
-          response = get("/base_level.industries.list.php", :query => {:page => page_num})
-
-          doc = Hpricot::XML(response)
+          doc = get_xml("/base_level.industries.list.php", :query => {:page => page_num})
 
           next_page = doc.search("/").first.attributes['next_page']
 
@@ -33,9 +50,8 @@ module GovKit
       def self.find(nimsp_id)
         next_page, result, page_num = "yes", [], 0
 
-        until next_page == "no"
-          response = get("/candidates.contributions.php", :query => {"imsp_candidate_id" => nimsp_id, :page => page_num})
-          doc = Hpricot::XML(response)
+        until next_page != "yes"
+          doc = get_xml("/candidates.contributions.php", :query => {"imsp_candidate_id" => nimsp_id, :page => page_num})
 
           next_page = doc.search("/").first.attributes['next_page']
 
@@ -49,8 +65,8 @@ module GovKit
       end
 
       def self.top(nimsp_id)
-        response = get("/candidates.top_contributor.php", :query => {"imsp_candidate_id" => nimsp_id})
-        doc = Hpricot::XML(response)
+        doc = get_xml("/candidates.top_contributor.php", :query => {"imsp_candidate_id" => nimsp_id})
+
         result = doc.search('//top_contributor').collect do |contribution|
           contribution.attributes.to_hash
         end
@@ -61,8 +77,7 @@ module GovKit
 
     class IndustryContribution < Contribution
       def self.find(nimsp_id)
-        response = get("/candidates.industries.php", :query => {"imsp_candidate_id" => nimsp_id})
-        doc = Hpricot::XML(response)
+        doc = get_xml("/candidates.industries.php", :query => {"imsp_candidate_id" => nimsp_id})
 
         result = doc.search('//candidate_industry').collect do |contribution|
           contribution.attributes.to_hash
@@ -74,8 +89,7 @@ module GovKit
 
     class SectorContribution < Contribution
       def self.find(nimsp_id)
-        response = get("/candidates.sectors.php", :query => {"imsp_candidate_id" => nimsp_id})
-        doc = Hpricot::XML(response)
+        doc = get_xml("/candidates.sectors.php", :query => {"imsp_candidate_id" => nimsp_id})
 
         result = doc.search('//candidate_sector').collect do |contribution|
           contribution.attributes.to_hash
@@ -87,8 +101,8 @@ module GovKit
 
     class BusinessContribution < Contribution
       def self.find(nimsp_id)
-        response = get("/candidates.businesses.php", :query => {"imsp_candidate_id" => nimsp_id})
-        doc = Hpricot::XML(response)
+        doc = get_xml("/candidates.businesses.php", :query => {"imsp_candidate_id" => nimsp_id})
+
         result = doc.search('//candidate_business').collect do |contribution|
           contribution.attributes.to_hash
         end
