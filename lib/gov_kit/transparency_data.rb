930932a124484d10c1070226c91f3d3bd4d7baf2
tashian <carl@ppolitics.org>
2010-12-07 12:28:10 -0800

    Add TransparencyData API Campaign Contributions support

---
+++ lib/gov_kit/transparency_data.rb
@@ -0,0 +1,18 @@
+module GovKit
+  class TransparencyDataResource < Resource
+    default_params :apikey => GovKit::configuration.sunlight_apikey
+    base_uri GovKit::configuration.transparency_data_base_url
+  end
+
+  module TransparencyData
+    # See http://transparencydata.com/api/contributions/
+    # for complete query options
+    class Contribution < TransparencyDataResource
+      def self.find(ops = {})
+        response = get('/contributions.json', :query => ops)
+        parse(response)
+      end
+    end
+  end
+
+end
